b3278b101a515bc5f871dd576408721a1aeeade7
Jet_29 <github@jetgames.space>
2023-12-09 19:16:05 +1100

    added time and host name to right side

--- src/theme.sh
+++ src/theme.sh
@@ -7,6 +7,7 @@ text_light="#ffffff"
 
 message_bg="#ff6c4d"
 message_fg="#16161e"
+message_text="${text_dark}"
 
 status_bg="#292E42"
 status_fg="#ffffff"
@@ -16,12 +17,23 @@ pane_border_active_bg="#16161e"
 
 session_bg="#55ff55"
 session_prefix_bg="#ffa800"
+session_text="${text_dark}"
+session_prefix_text="${text_dark}"
 
-active_window_id_bg="#5555ff"
-active_window_bg="#3333ff"
+active_window_id_bg="#6666ff"
+active_window_bg="#4444ff"
+active_window_text="${text_light}"
 
 inactive_window_id_bg="#737aa2"
 inactive_window_bg="#545C7e"
+inactive_window_text="${text_light}"
+
+computer_name_bg="#4444ff"
+computer_name_text="${text_light}"
+
+date_time_bg="#55ff55"
+date_time_text="${text_dark}"
+
 
 # Options
 left_separator=""
@@ -41,7 +53,9 @@ tmux set-option -g pane-active-border-style "bg=${pane_border_active_bg},fg=${st
 tmux set-option -g pane-border-style "bg=${pane_border_bg},fg=${status_bg}"
 
 # Left side
-left_side="#[fg=${text_dark},bold]#{?client_prefix,#[bg=${session_prefix_bg}],#[bg=${session_bg}]}"
+session_prefixed="#[bg=${session_prefix_bg}#,fg=${session_prefix_text}#,bold]"
+session_non_prefixed="#[bg=${session_bg}#,fg=${session_text}#,bold]}"
+left_side="#{?client_prefix,${session_prefixed},${session_non_prefixed}"
 left_side+=" #S "
 left_side+="#[bg=${status_bg}]#{?client_prefix,#[fg=${session_prefix_bg}],#[fg=${session_bg}]}"
 left_side+="${left_separator}"
@@ -54,21 +68,28 @@ separator_start="#[bg=${inactive_window_id_bg},fg=${status_bg}]${left_separator}
 separator_internal="#[bg=${inactive_window_bg},fg=${inactive_window_id_bg}]${left_separator}#[none]"
 separator_end="#[bg=${status_bg},fg=${inactive_window_bg}]${left_separator}#[none]"
 
-inactive_window="${separator_start}#[fg=${text_light}]#I${separator_internal}#[fg=${text_light}] #W ${separator_end}"
+inactive_window="${separator_start}#[fg=${inactive_window_text}]#I${separator_internal}#[fg=${inactive_window_text}] #W ${separator_end}"
 
 # # Active
 separator_start="#[bg=${active_window_id_bg},fg=${status_bg}]${left_separator}#[none]"
 separator_internal="#[bg=${active_window_bg},fg=${active_window_id_bg}]${left_separator}#[none]"
 separator_end="#[bg=${status_bg},fg=${active_window_bg}]${left_separator}#[none]"
 
-active_window="${separator_start}#[fg=${text_light}]#I${separator_internal}#[fg=${text_light}] #W ${separator_end}"
+active_window="${separator_start}#[fg=${active_window_text}]#I${separator_internal}#[fg=${active_window_text}] #W ${separator_end}"
 
 tmux set-window-option -g window-status-format "${inactive_window}" 
 tmux set-window-option -g window-status-current-format "${active_window}"
 
-# TODO: add plugin stuff to the right side like time and cpu usage maybe.
 # Right side
-# tmux set-option -g status-right ""
+# Host name
+right_side="#[bg=${status_bg},fg=${computer_name_bg}]${right_separator}#[none]"
+right_side+="#[bg=${computer_name_bg},fg=${computer_name_text}] #H #[none]"
+right_side+="#[bg=${computer_name_bg},fg=${status_bg}]${right_separator}#[none]"
 
+# Date Time
+right_side+="#[bg=${status_bg},fg=${date_time_bg}]${right_separator}#[none]"
+right_side+="#[bg=${date_time_bg},fg=${date_time_text}] %a, %d %b %I:%M #[none]"
 
-# tmux set-window-option -g window-status-separator ""
+tmux set-option -g status-right "${right_side}"
+
+tmux set-window-option -g window-status-separator ""
